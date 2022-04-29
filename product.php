7f2648a7cda5fa883e1d444a9c747a579f37447a
Sczeam <zawhtikeaung19@gmail.com>
2022-04-29 23:07:05 +0630

    product page php file

---
+++ product.php
@@ -0,0 +1,39 @@
+<?php include('header.php'); ?>
+
+    <!--
+      - BANNER
+    -->
+   
+
+  
+
+    <!--
+      - PRODUCT
+    -->
+
+    <div class="product-container">
+
+      <div class="container">
+
+
+        <!--
+          - SIDEBAR
+        -->
+        <!-- best seller -->
+        <?php 
+        
+          include('template/_best-seller.php');
+
+          include('template/_all-products.php');
+        
+        ?>
+
+        
+      </div>
+
+    </div>
+
+    
+
+    <?php include('footer.php'); ?>
+ 
