71aac775204e665e93d8f7f167953d8004c9e4a5
Sczeam <zawhtikeaung19@gmail.com>
2022-05-01 01:37:04 +0630

    login register form frontend

--- assets/js/script.js
+++ assets/js/script.js
@@ -86,3 +86,50 @@ for (let i = 0; i < accordionBtn.length; i++) {
   });
 
 }
+
+const inputs = document.querySelectorAll(".input-field");
+const toggle_btn = document.querySelectorAll(".toggle");
+const main = document.querySelector("main");
+const bullets = document.querySelectorAll(".bullets span");
+const images = document.querySelectorAll(".image");
+
+inputs.forEach((inp) => {
+  inp.addEventListener("focus", () => {
+    inp.classList.add("active");
+  });
+  inp.addEventListener("blur", () => {
+    if (inp.value != "") return;
+    inp.classList.remove("active");
+  });
+});
+
+toggle_btn.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    main.classList.toggle("sign-up-mode");
+  });
+});
+
+function moveSlider() {
+  let index = this.dataset.value;
+
+  let currentImage = document.querySelector(`.img-${index}`);
+  images.forEach((img) => img.classList.remove("show"));
+  currentImage.classList.add("show");
+
+  const textSlider = document.querySelector(".text-group");
+  textSlider.style.transform = `translateY(${-(index - 1) * 2.2}rem)`;
+
+  bullets.forEach((bull) => bull.classList.remove("active"));
+  this.classList.add("active");
+}
+
+bullets.forEach((bullet) => {
+  bullet.addEventListener("click", moveSlider);
+});
+
+document.querySelector("#show-login").addEventListener("click", function(){
+    document.querySelector(".box").classList.add("active")
+});
+document.querySelector("[data-login-close]").addEventListener("click", function(){
+    document.querySelector(".box").classList.remove("active")
+});
